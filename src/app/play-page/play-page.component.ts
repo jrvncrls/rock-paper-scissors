3ea0f3be3ecde330329473c8af41c791f719441f
jervincarlos <jervin@sliqpay.com>
2022-11-11 21:22:08 +0800

    Add opponents choice

--- src/app/play-page/play-page.component.ts
+++ src/app/play-page/play-page.component.ts
@@ -3,13 +3,67 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-play-page',
   templateUrl: './play-page.component.html',
-  styleUrls: ['./play-page.component.scss']
+  styleUrls: ['./play-page.component.scss'],
 })
 export class PlayPageComponent implements OnInit {
+  Choice = Choice;
 
-  constructor() { }
+  timer = 3;
+
+  opponentsChoice!: string;
+  playersChoice!: Choice;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.opponentsChoice = this.setOpponentsChoice();
+
+    let interval = setInterval(() => {
+      this.timer--;
+      if (this.timer === 0) {
+        clearInterval(interval);
+        console.log(
+          this.calculateChoices(this.playersChoice, this.opponentsChoice)
+        );
+      }
+    }, 1000);
   }
 
+  setPlayersChoice(choice: Choice): void {
+    this.playersChoice = choice;
+  }
+
+  setOpponentsChoice(): string {
+    return Object.values(Choice)[Math.floor(Math.random() * 3)];
+  }
+
+  calculateChoices(playerChoice: Choice, opponentsChoice: string): Result {
+    // Draw
+    if (playerChoice === opponentsChoice) {
+      return Result.Won;
+    }
+
+    // Won
+    if (
+      (playerChoice === Choice.Rock && opponentsChoice === Choice.Scissor) ||
+      (playerChoice === Choice.Paper && opponentsChoice === Choice.Rock) ||
+      (playerChoice === Choice.Scissor && opponentsChoice === Choice.Paper)
+    ) {
+      return Result.Won;
+    }
+
+    return Result.Lose;
+  }
+}
+
+export enum Choice {
+  Rock = 'rock',
+  Paper = 'paper',
+  Scissor = 'scissors',
+}
+
+enum Result {
+  Won = 'You Won',
+  Lose = 'You Lose',
+  Draw = 'Draw',
 }
